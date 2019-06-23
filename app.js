a651797f5cf9c0765eb31d9e18ba6e315a424bd2
smukhiyabeen <smukhiyabeen@gmail.com>
2019-06-22 21:27:52 -0400

    add route for results page

--- app.js
+++ app.js
@@ -2,5 +2,17 @@ const express = require('express');
 const app = express();
 const request = require('request');
 const port = 8080;
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+
+app.get('/results', (req, res) => {
+    request(`http://www.omdbapi.com/?s=cat&apikey=${process.env.apiKey}`, (error, response, body) => {
+        let results = JSON.parse(body)
+        if (!error && response.statusCode == 200) {
+            res.send(results.Search[0])
+        }
+    });
+});
 
 app.listen(port, () => console.log(`Movie app started on port: ${port}`))
