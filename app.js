da7d01cd83ae6fe8780a34fb739523eae1b91640
smukhiyabeen <smukhiyabeen@gmail.com>
2019-06-22 22:24:50 -0400

    added a search homepage which allows searching for movies

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const express = require('express');
 const app = express();
 const request = require('request');
 const port = 8080;
+
 require('dotenv').config();
 
 app.set('view engine', 'ejs');
 
+
+app.get('/', (req,res) => {
+    res.render('search')
+});
+
 app.get('/results', (req, res) => {
-    request(`http://www.omdbapi.com/?s=cat&apikey=${process.env.apiKey}`, (error, response, body) => {
-        let results = JSON.parse(body)
+    let search = req.query.search
+    let url = `http://www.omdbapi.com/?s=${search}&apikey=${process.env.apiKey}`
+    request(url, (error, response, body) => {
+        let data = JSON.parse(body);
         if (!error && response.statusCode == 200) {
-            res.send(results.Search[0])
+            res.render("results", {data: data});
         }
     });
 });
